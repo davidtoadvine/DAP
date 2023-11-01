4b5266b31bfbf8644a70483bfb1a0981fa042330
David Toadvine <davidtoadvine@proton.me>
2023-11-01 14:00:16 -0400

    formatting

--- script/notes.js
+++ script/notes.js
@@ -3,11 +3,11 @@ const createButton = document.querySelector(".note-btn");
 let notes = document.querySelectorAll(".input-box");
 showNotes();
 
-function showNotes(){
+function showNotes() {
   notesContainer.innerHTML = localStorage.getItem("notes");
-  
+
 }
-function updateStorage(){
+function updateStorage() {
   localStorage.setItem("notes", notesContainer.innerHTML);
 }
 createButton.addEventListener("click", (e) => {
@@ -17,23 +17,23 @@ createButton.addEventListener("click", (e) => {
   inputBox.setAttribute("contenteditable", "true");
   //this works but isn't perfect
   inputBox.innerHTML = '&nbsp';
-  
+
   notesContainer.appendChild(inputBox);
   img.src = "../images/trash.png";
   img.alt = "Delete note"
   inputBox.appendChild(img);
 });
-notesContainer.addEventListener("click", function(e){
-  if(e.target.tagName === "IMG"){
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
     ///delete image
     e.target.parentElement.remove();
     updateStorage();
   }
-  else if(e.target.tagName === "P"){
-    
+  else if (e.target.tagName === "P") {
+
     notes = document.querySelectorAll(".input-box");
     notes.forEach(note => {
-      note.onkeyup = function(){
+      note.onkeyup = function () {
         updateStorage();
       }
     })
