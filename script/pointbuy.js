6982aa5c67e0a26542746123971b7b6ba8ad3dfd
David Toadvine <davidtoadvine@proton.me>
2023-11-23 10:16:44 -0500

    sloppy pb buttons

--- script/pointbuy.js
+++ script/pointbuy.js
@@ -32,11 +32,173 @@ $('.btn-number1').click(function (e) {
   }
 });
 
-$('.input-number').focusin(function () {
+//when btn-number class member is clicked
+$('.btn-number2').click(function (e) {
+  e.preventDefault();
+  fieldName = $(this).attr('data-field');
+  console.log(fieldName);
+  type = $(this).attr('data-type');
+  var input = $("input[name='" + fieldName + "']");
+  var currentVal = parseInt(input.val());
+  if (!isNaN(currentVal)) {
+    if (type == 'minus') {
+      if (currentVal > input.attr('min')) {
+        input.val(currentVal - 1).change();
+
+
+      }
+      if (parseInt(input.val()) == input.attr('min')) {
+        $(this).attr('disabled', true);
+      }
+    } else if (type == 'plus') {
+      if (currentVal < input.attr('max')) {
+        input.val(currentVal + 1).change();
+
+
+      }
+      if (parseInt(input.val()) == input.attr('max')) {
+        $(this).attr('disabled', true);
+      }
+    }
+  } else {
+    input.val(8);
+  }
+});
+
+//when btn-number class member is clicked
+$('.btn-number3').click(function (e) {
+  e.preventDefault();
+  fieldName = $(this).attr('data-field');
+  console.log(fieldName);
+  type = $(this).attr('data-type');
+  var input = $("input[name='" + fieldName + "']");
+  var currentVal = parseInt(input.val());
+  if (!isNaN(currentVal)) {
+    if (type == 'minus') {
+      if (currentVal > input.attr('min')) {
+        input.val(currentVal - 1).change();
+
+
+      }
+      if (parseInt(input.val()) == input.attr('min')) {
+        $(this).attr('disabled', true);
+      }
+    } else if (type == 'plus') {
+      if (currentVal < input.attr('max')) {
+        input.val(currentVal + 1).change();
+
+
+      }
+      if (parseInt(input.val()) == input.attr('max')) {
+        $(this).attr('disabled', true);
+      }
+    }
+  } else {
+    input.val(8);
+  }
+});
+//when btn-number class member is clicked
+$('.btn-number4').click(function (e) {
+  e.preventDefault();
+  fieldName = $(this).attr('data-field');
+  console.log(fieldName);
+  type = $(this).attr('data-type');
+  var input = $("input[name='" + fieldName + "']");
+  var currentVal = parseInt(input.val());
+  if (!isNaN(currentVal)) {
+    if (type == 'minus') {
+      if (currentVal > input.attr('min')) {
+        input.val(currentVal - 1).change();
+
+
+      }
+      if (parseInt(input.val()) == input.attr('min')) {
+        $(this).attr('disabled', true);
+      }
+    } else if (type == 'plus') {
+      if (currentVal < input.attr('max')) {
+        input.val(currentVal + 1).change();
+
+
+      }
+      if (parseInt(input.val()) == input.attr('max')) {
+        $(this).attr('disabled', true);
+      }
+    }
+  } else {
+    input.val(8);
+  }
+});
+//when btn-number class member is clicked
+$('.btn-number5').click(function (e) {
+  e.preventDefault();
+  fieldName = $(this).attr('data-field');
+  console.log(fieldName);
+  type = $(this).attr('data-type');
+  var input = $("input[name='" + fieldName + "']");
+  var currentVal = parseInt(input.val());
+  if (!isNaN(currentVal)) {
+    if (type == 'minus') {
+      if (currentVal > input.attr('min')) {
+        input.val(currentVal - 1).change();
+
+
+      }
+      if (parseInt(input.val()) == input.attr('min')) {
+        $(this).attr('disabled', true);
+      }
+    } else if (type == 'plus') {
+      if (currentVal < input.attr('max')) {
+        input.val(currentVal + 1).change();
+
+
+      }
+      if (parseInt(input.val()) == input.attr('max')) {
+        $(this).attr('disabled', true);
+      }
+    }
+  } else {
+    input.val(8);
+  }
+});
+//when btn-number class member is clicked
+$('.btn-number6').click(function (e) {
+  e.preventDefault();
+  fieldName = $(this).attr('data-field');
+  console.log(fieldName);
+  type = $(this).attr('data-type');
+  var input = $("input[name='" + fieldName + "']");
+  var currentVal = parseInt(input.val());
+  if (!isNaN(currentVal)) {
+    if (type == 'minus') {
+      if (currentVal > input.attr('min')) {
+        input.val(currentVal - 1).change();
+
+
+      }
+      if (parseInt(input.val()) == input.attr('min')) {
+        $(this).attr('disabled', true);
+      }
+    } else if (type == 'plus') {
+      if (currentVal < input.attr('max')) {
+        input.val(currentVal + 1).change();
+
+
+      }
+      if (parseInt(input.val()) == input.attr('max')) {
+        $(this).attr('disabled', true);
+      }
+    }
+  } else {
+    input.val(8);
+  }
+});
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+$('.input-number1').focusin(function () {
   $(this).data('oldValue', $(this).val());
 });
 
-$('.input-number').change(function () {
+$('.input-number1').change(function () {
   minValue = parseInt($(this).attr('min'));
   maxValue = parseInt($(this).attr('max'));
   valueCurrent = parseInt($(this).val());
@@ -64,7 +226,151 @@ $('.input-number').change(function () {
   }
 });
 
-$(".input-number").keydown(function (e) {
+$(".input-number1").keydown(function (e) {
+  // Allow: backspace, delete, tab, escape, enter and .
+  if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+    // Allow: Ctrl+A
+    (e.keyCode == 65 && e.ctrlKey === true) ||
+    // Allow: home, end, left, right
+    (e.keyCode >= 35 && e.keyCode <= 39)) {
+    // let it happen, don't do anything
+    return;
+  }
+  // Ensure that it is a number and stop the keypress
+  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+    e.preventDefault();
+  }
+});
+/////////////////////////////////////////////////
+$('.input-number2').focusin(function () {
+  $(this).data('oldValue', $(this).val());
+});
+
+$('.input-number2').change(function () {
+  minValue = parseInt($(this).attr('min'));
+  maxValue = parseInt($(this).attr('max'));
+  valueCurrent = parseInt($(this).val());
+  name = $(this).attr('name');
+  if (valueCurrent >= minValue) {
+    $(".btn-number2[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+
+  } else {
+    alert('Sorry, the minimum value was reached');
+    $(this).val(8);
+
+  }
+  if (valueCurrent <= maxValue && valueCurrent > 0) {
+    $(".btn-number2[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else if (valueCurrent <= maxValue) {
+    $(".btn-number2[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else {
+    alert('Sorry, the maximum value was reached');
+    $(this).val(15);
+
+  }
+});
+
+$(".input-number2").keydown(function (e) {
+  // Allow: backspace, delete, tab, escape, enter and .
+  if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+    // Allow: Ctrl+A
+    (e.keyCode == 65 && e.ctrlKey === true) ||
+    // Allow: home, end, left, right
+    (e.keyCode >= 35 && e.keyCode <= 39)) {
+    // let it happen, don't do anything
+    return;
+  }
+  // Ensure that it is a number and stop the keypress
+  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+    e.preventDefault();
+  }
+});
+///////////////////////////////////////////
+$('.input-number3').focusin(function () {
+  $(this).data('oldValue', $(this).val());
+});
+
+$('.input-number3').change(function () {
+  minValue = parseInt($(this).attr('min'));
+  maxValue = parseInt($(this).attr('max'));
+  valueCurrent = parseInt($(this).val());
+  name = $(this).attr('name');
+  if (valueCurrent >= minValue) {
+    $(".btn-number3[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+
+  } else {
+    alert('Sorry, the minimum value was reached');
+    $(this).val(8);
+
+  }
+  if (valueCurrent <= maxValue && valueCurrent > 0) {
+    $(".btn-number3[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else if (valueCurrent <= maxValue) {
+    $(".btn-number3[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else {
+    alert('Sorry, the maximum value was reached');
+    $(this).val(15);
+
+  }
+});
+
+$(".input-number3").keydown(function (e) {
+  // Allow: backspace, delete, tab, escape, enter and .
+  if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+    // Allow: Ctrl+A
+    (e.keyCode == 65 && e.ctrlKey === true) ||
+    // Allow: home, end, left, right
+    (e.keyCode >= 35 && e.keyCode <= 39)) {
+    // let it happen, don't do anything
+    return;
+  }
+  // Ensure that it is a number and stop the keypress
+  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+    e.preventDefault();
+  }
+});
+///////////////////////////////////////////
+$('.input-number4').focusin(function () {
+  $(this).data('oldValue', $(this).val());
+});
+
+$('.input-number4').change(function () {
+  minValue = parseInt($(this).attr('min'));
+  maxValue = parseInt($(this).attr('max'));
+  valueCurrent = parseInt($(this).val());
+  name = $(this).attr('name');
+  if (valueCurrent >= minValue) {
+    $(".btn-number4[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+
+  } else {
+    alert('Sorry, the minimum value was reached');
+    $(this).val(8);
+
+  }
+  if (valueCurrent <= maxValue && valueCurrent > 0) {
+    $(".btn-number4[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else if (valueCurrent <= maxValue) {
+    $(".btn-number4[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else {
+    alert('Sorry, the maximum value was reached');
+    $(this).val(15);
+
+  }
+});
+
+$(".input-number4").keydown(function (e) {
   // Allow: backspace, delete, tab, escape, enter and .
   if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
     // Allow: Ctrl+A
@@ -80,7 +386,206 @@ $(".input-number").keydown(function (e) {
   }
 });
 
-/////////////////////
+///////////////////////////////////////////
+$('.input-number5').focusin(function () {
+  $(this).data('oldValue', $(this).val());
+});
+
+$('.input-number5').change(function () {
+  minValue = parseInt($(this).attr('min'));
+  maxValue = parseInt($(this).attr('max'));
+  valueCurrent = parseInt($(this).val());
+  name = $(this).attr('name');
+  if (valueCurrent >= minValue) {
+    $(".btn-number5[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+
+  } else {
+    alert('Sorry, the minimum value was reached');
+    $(this).val(8);
+
+  }
+  if (valueCurrent <= maxValue && valueCurrent > 0) {
+    $(".btn-number5[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else if (valueCurrent <= maxValue) {
+    $(".btn-number5[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else {
+    alert('Sorry, the maximum value was reached');
+    $(this).val(15);
+
+  }
+});
+
+$(".input-number5").keydown(function (e) {
+  // Allow: backspace, delete, tab, escape, enter and .
+  if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+    // Allow: Ctrl+A
+    (e.keyCode == 65 && e.ctrlKey === true) ||
+    // Allow: home, end, left, right
+    (e.keyCode >= 35 && e.keyCode <= 39)) {
+    // let it happen, don't do anything
+    return;
+  }
+  // Ensure that it is a number and stop the keypress
+  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+    e.preventDefault();
+  }
+});
+///////////////////////////////////////////
+$('.input-number6').focusin(function () {
+  $(this).data('oldValue', $(this).val());
+});
+
+$('.input-number6').change(function () {
+  minValue = parseInt($(this).attr('min'));
+  maxValue = parseInt($(this).attr('max'));
+  valueCurrent = parseInt($(this).val());
+  name = $(this).attr('name');
+  if (valueCurrent >= minValue) {
+    $(".btn-number2[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+
+  } else {
+    alert('Sorry, the minimum value was reached');
+    $(this).val(8);
+
+  }
+  if (valueCurrent <= maxValue && valueCurrent > 0) {
+    $(".btn-number6[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else if (valueCurrent <= maxValue) {
+    $(".btn-number6[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else {
+    alert('Sorry, the maximum value was reached');
+    $(this).val(15);
+
+  }
+});
+
+$(".input-number6").keydown(function (e) {
+  // Allow: backspace, delete, tab, escape, enter and .
+  if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+    // Allow: Ctrl+A
+    (e.keyCode == 65 && e.ctrlKey === true) ||
+    // Allow: home, end, left, right
+    (e.keyCode >= 35 && e.keyCode <= 39)) {
+    // let it happen, don't do anything
+    return;
+  }
+  // Ensure that it is a number and stop the keypress
+  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+    e.preventDefault();
+  }
+});
+///////////////////////////////////////////
+$('.input-number2').focusin(function () {
+  $(this).data('oldValue', $(this).val());
+});
+
+$('.input-number2').change(function () {
+  minValue = parseInt($(this).attr('min'));
+  maxValue = parseInt($(this).attr('max'));
+  valueCurrent = parseInt($(this).val());
+  name = $(this).attr('name');
+  if (valueCurrent >= minValue) {
+    $(".btn-number2[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+
+  } else {
+    alert('Sorry, the minimum value was reached');
+    $(this).val(8);
+
+  }
+  if (valueCurrent <= maxValue && valueCurrent > 0) {
+    $(".btn-number2[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else if (valueCurrent <= maxValue) {
+    $(".btn-number2[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else {
+    alert('Sorry, the maximum value was reached');
+    $(this).val(15);
+
+  }
+});
+
+$(".input-number2").keydown(function (e) {
+  // Allow: backspace, delete, tab, escape, enter and .
+  if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+    // Allow: Ctrl+A
+    (e.keyCode == 65 && e.ctrlKey === true) ||
+    // Allow: home, end, left, right
+    (e.keyCode >= 35 && e.keyCode <= 39)) {
+    // let it happen, don't do anything
+    return;
+  }
+  // Ensure that it is a number and stop the keypress
+  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+    e.preventDefault();
+  }
+});
+///////////////////////////////////////////
+$('.input-number2').focusin(function () {
+  $(this).data('oldValue', $(this).val());
+});
+
+$('.input-number2').change(function () {
+  minValue = parseInt($(this).attr('min'));
+  maxValue = parseInt($(this).attr('max'));
+  valueCurrent = parseInt($(this).val());
+  name = $(this).attr('name');
+  if (valueCurrent >= minValue) {
+    $(".btn-number2[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+
+  } else {
+    alert('Sorry, the minimum value was reached');
+    $(this).val(8);
+
+  }
+  if (valueCurrent <= maxValue && valueCurrent > 0) {
+    $(".btn-number2[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else if (valueCurrent <= maxValue) {
+    $(".btn-number2[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+
+  }
+  else {
+    alert('Sorry, the maximum value was reached');
+    $(this).val(15);
+
+  }
+});
+
+$(".input-number2").keydown(function (e) {
+  // Allow: backspace, delete, tab, escape, enter and .
+  if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+    // Allow: Ctrl+A
+    (e.keyCode == 65 && e.ctrlKey === true) ||
+    // Allow: home, end, left, right
+    (e.keyCode >= 35 && e.keyCode <= 39)) {
+    // let it happen, don't do anything
+    return;
+  }
+  // Ensure that it is a number and stop the keypress
+  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+    e.preventDefault();
+  }
+});
+
+
+
+
+
+
+
+///////////////////// THIS IS MODIFYING MODIFIER
 let strBase = document.getElementById("str-form").innerHTML;
 
 let strPlus = document.getElementById("str-plus");
@@ -88,12 +593,9 @@ let strPlus = document.getElementById("str-plus");
 let strMinus = document.getElementById("str-minus");
 
 strPlus.onclick = function () {
+  
 
-  console.log(document.getElementById('quantity').value);
-
-  document.getElementById('str-mod').innerHTML = document.getElementById('quantity').value;
+  document.getElementById('str-mod').innerHTML = document.getElementById('quantity-str').value;
 
 }
-
-
 
---
+++ script/spellbook.js
@@ -0,0 +1,201 @@
+
+//reset class dropdown on refresh
+const classSelect = document.getElementById('class-select');
+classSelect.value = 0;
+
+// getting all spells as default option on load
+async function fetchAllSpells() {
+  try {
+    const response = await fetch(`https://www.dnd5eapi.co/api/spells`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    const spellNames = data.results.map((spell) => spell.name);
+    // put class spells into spell dropdown
+    populateDropdown(spellNames);
+  } catch (error) {
+    console.error(`Error fetching all spells:`, error.message);
+  }
+}
+fetchAllSpells();
+
+
+// narrowing spells based on class input
+async function fetchClassSpells(className) {
+  try {
+
+    const response = await fetch(`https://www.dnd5eapi.co/api/classes/${className}/spells`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const spellNames = data.results.map((spell) => spell.name);
+
+    // put class spells into spell dropdown
+    populateDropdown(spellNames);
+
+  } catch (error) {
+    console.error(`Error fetching ${className} spells:`, error.message);
+  }
+}
+
+//populating the spell dropdown menu
+function populateDropdown(spellNames) {
+  console.log("populating with spell names")
+  console.log(spellNames.length);
+  const spellSelect = document.getElementById('spell-select');
+  // Clear existing options
+  spellSelect.innerHTML = '';
+
+
+  // if no spells are found (this shouldn't occur though)
+  if (spellNames.length == 0) {
+    // Add a default option
+    const defaultOption = document.createElement('option');
+    defaultOption.text = 'No spells available for this class.';
+    spellSelect.add(defaultOption);
+
+    const spellBox = document.getElementById("spell-info");
+    // Clear existing
+    spellBox.innerHTML = '';
+
+    const martialText = document.createElement('h3');
+    spellBox.appendChild(martialText);
+    martialText.textContent = "No spells available for this class.";
+
+  }
+
+  // if spells are found
+  else {
+    // Add a default option
+    const defaultOption = document.createElement('option');
+    defaultOption.text = 'Select a spell';
+    spellSelect.add(defaultOption);
+
+    // Add spell names to the dropdown
+    spellNames.forEach((spellName) => {
+      const option = document.createElement('option');
+      option.text = spellName;
+      spellSelect.add(option);
+    });
+  }
+}
+
+
+// getting spell data
+async function fetchSpellDetails(spell) {
+  try {
+    // format text for url
+    spell = spell.replace(/ /g, '-');
+
+    const response = await fetch(`https://www.dnd5eapi.co/api/spells/${spell}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    /// all the pieces of spell data
+    const Name = data.name;
+    const Desc = data.desc;
+    const Range = data.range;
+    const Components = data.components;
+    const Material = data.material;
+    const Ritual = data.ritual;
+    const Duration = data.duration;
+    const Concentration = data.concentration;
+    const CastingTime = data.casting_time;
+    const Level = data.level;
+    const School = data.school.name;
+
+    //text box
+    const spellBox = document.getElementById("spell-info");
+
+    // Clear existing
+    spellBox.innerHTML = '';
+
+    // Name
+    const spellName = document.createElement('h2');
+    spellBox.appendChild(spellName);
+    const strongElement = document.createElement('strong');
+    strongElement.textContent = Name;
+    spellName.appendChild(strongElement);
+
+    // Description
+    const spellDesc = document.createElement('h3');
+
+    Desc.forEach((line) => {
+      const h = document.createElement('h3');
+      h.textContent = line;
+      spellBox.appendChild(h);
+    })
+
+
+    // Range
+    const spellRange = document.createElement('h3');
+    spellBox.appendChild(spellRange);
+    spellRange.appendChild(document.createElement('strong')).textContent = 'Range:';
+    spellRange.appendChild(document.createTextNode(` ${Range}`));
+
+    // Components
+    const spellComponents = document.createElement('h3');
+    spellBox.appendChild(spellComponents);
+    spellComponents.appendChild(document.createElement('strong')).textContent = 'Components:';
+    spellComponents.appendChild(document.createTextNode(` ${Components}`));
+
+    // Material
+    const spellMaterial = document.createElement('h3');
+    spellBox.appendChild(spellMaterial);
+    spellMaterial.appendChild(document.createElement('strong')).textContent = 'Material:';
+    spellMaterial.appendChild(document.createTextNode(` ${Material || 'None'}`));
+
+    // Duration
+    const spellDuration = document.createElement('h3');
+    spellBox.appendChild(spellDuration);
+    spellDuration.appendChild(document.createElement('strong')).textContent = 'Duration:';
+    spellDuration.appendChild(document.createTextNode(` ${Duration}`));
+
+    // Concentration
+    const spellConcentration = document.createElement('h3');
+    spellBox.appendChild(spellConcentration);
+    spellConcentration.appendChild(document.createElement('strong')).textContent = 'Concentration:';
+    spellConcentration.appendChild(document.createTextNode(` ${Concentration ? 'Yes' : 'No'}`));
+
+    // Casting Time
+    const spellCastingTime = document.createElement('h3');
+    spellBox.appendChild(spellCastingTime);
+    spellCastingTime.appendChild(document.createElement('strong')).textContent = 'Casting Time:';
+    spellCastingTime.appendChild(document.createTextNode(` ${CastingTime}`));
+
+    // Level
+    const spellLevel = document.createElement('h3');
+    spellBox.appendChild(spellLevel);
+    spellLevel.appendChild(document.createElement('strong')).textContent = 'Level:';
+    spellLevel.appendChild(document.createTextNode(` ${Level}`));
+
+    // School
+    const spellSchool = document.createElement('h3');
+    spellBox.appendChild(spellSchool);
+    spellSchool.appendChild(document.createElement('strong')).textContent = 'School:';
+    spellSchool.appendChild(document.createTextNode(` ${School}`));
+
+  } catch (error) {
+    console.error(`Error fetching ${spell} data:`, error.message);
+  }
+}
+
+// Event listener for class dropdown change
+document.getElementById('class-select').addEventListener('change', async function () {
+  const selectedClassName = this.options[this.selectedIndex].textContent.toLowerCase();
+  await fetchClassSpells(selectedClassName);
+})
+
+// Event listener for spell dropdown change
+document.getElementById('spell-select').addEventListener('change', async function () {
+  const selectedSpellName = this.value.toLowerCase();
+  await fetchSpellDetails(selectedSpellName);
+})
